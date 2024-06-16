e8dfa2326c5e81013ae32fef83b55f7fae42e395
Ka3zar <al3x00prc@gmail.com>
2024-06-16 11:44:08 +0200

    comment

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const sharp = require('sharp'); //maybe you don't need it
+//const sharp = require('sharp'); // !!! maybe you don't need it !!!
 const app = express()
 const port = 3000
 
